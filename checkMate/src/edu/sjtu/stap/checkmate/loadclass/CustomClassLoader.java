77c3ef9f7365b2bb3bba132b3fff5d39b2995bd7
MondayLei <zhouyilei0318@gmail.com>
2014-12-08 22:39:17 +0800

    Custom Classloader, currently can load .class from class path.

--- checkMate/src/edu/sjtu/stap/checkmate/loadclass/CustomClassLoader.java
+++ checkMate/src/edu/sjtu/stap/checkmate/loadclass/CustomClassLoader.java
@@ -1,6 +1,7 @@
 package edu.sjtu.stap.checkmate.loadclass;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
@@ -11,36 +12,86 @@ import java.net.URL;
  *
  */
 public class CustomClassLoader extends ClassLoader {
+	private String[] classpath;
+
+	private final ClassLoader parent;
+
 	// private final ClassLoader parent;
 	private static int count = 0;
 
-	/*
-	 * Set parent classloader to bootstrap. 
-	 */
-	public CustomClassLoader() {
-		super(null);
-	}
-	
 	public CustomClassLoader(ClassLoader parent) {
 		super(parent);
+		this.parent = parent;
+		classpath = System.getProperty("java.class.path").split(";");
 	}
 
 	@Override
-	public synchronized Class<?> loadClass(String name) throws ClassNotFoundException {
-		System.out.println("Loading class :" + name + "; Count = " + count++);
-		
+	public synchronized Class<?> loadClass(String name)
+			throws ClassNotFoundException {
 		// First, check if the class has already been loaded
 		Class clazz = findLoadedClass(name);
-		
-		if ( clazz == null ) {
-			// try load from bootstrap 
+
+		if (clazz != null)
+			return clazz;
+
+		// Load class from classpath
+		if ( name.contains("edu"))
+			clazz = loadClassFromClasspath(name);
+		if (clazz != null) {
+			System.out.println("Load Class " + name + " from classpath.");
+			return clazz;
 		}
-		if (clazz == null ) {
+
+		// Load class from jar
+		// clazz = loadClassFromJar(name);
+		if (clazz != null) {
 			// Define a new class with instrumentation.
 		}
-		return clazz;
+		// this.defineClass(name, b, off, len)
+		return super.loadClass(name);
 	}
 
+	/**
+	 * Manually load class from classpath
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private Class loadClassFromClasspath(String name) {
+//		for (String s : classpath) {
+//			System.out.println("Classpath: " + s);
+//		}
+
+		String slashName = name.replace(".", "\\");
+		//for (String s : classpath) {
+			// String fullQualifiedName = s + "\\" + slashName + ".class";
+			// Need NOT Full Path
+			String fullQualifiedName = "\\" + slashName + ".class";
+
+			System.out.println("Load Class: " + fullQualifiedName);
+
+			try {
+				InputStream is = getResourceAsStream(fullQualifiedName);
+				if (is != null) {
+					ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+					int data;
+					data = is.read();
+					while (data != -1) {
+						buffer.write(data);
+						data = is.read();
+					}
+					is.close();
+					byte[] classData = buffer.toByteArray();
+					return defineClass(name, classData, 0, classData.length);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				// e.printStackTrace();
+			}
+
+		//}
+		return null;
+	}
 
 	private byte[] getClassData(String name) {
 
