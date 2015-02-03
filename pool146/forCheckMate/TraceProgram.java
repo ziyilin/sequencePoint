e9d905bd8fd28323d177bfc58b484e8eb1e4c7fa
ziyilin <lin.ziyi@hotmail.com>
2015-02-03 20:00:15 +0800

    for jpf

--- pool146/forCheckMate/TraceProgram.java
+++ pool146/forCheckMate/TraceProgram.java
@@ -1,406 +1,179 @@
-public class TraceProgram{
-static Object l1888626692=new Object();
-static Object l899130236=new Object();
-static Object l1948780723=new Object();
-static boolean c768288241;
-static boolean c1970826852;
-static boolean c1884511064;
-static boolean c1312737936;
-static Thread t1=new Thread()
-{ public void run(){c1312737936=false;
-synchronized(l4461550){
-}
-
-synchronized(l4461550){
-synchronized(l4461550){
-}
-
-}
-
-synchronized(l4461550){
-synchronized(l4461550){
-}
-
-}
-
-synchronized(l4461550){
-synchronized(l4461550){
-}
-
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l899130236){
-}
-
-c1970826852=true;
-synchronized(l4461550){
-synchronized(l4461550){
-synchronized(l899130236){
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l899130236){
-}
-
-if(c1312737936){
-synchronized(l899130236){
-l899130236.notify();
-}
-
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l1986936160){
-}
-
-synchronized(l899130236){
-l899130236.notify();
-}
-
-}
-
-}
-
-synchronized(l4461550){
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l899130236){
-}
-
-if(c1970826852){
-synchronized(l899130236){
-try{l899130236.wait();
-}catch(InterruptedException e){e.printStackTrace();}
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l899130236){
-}
-
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l1065330270){
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l4461550){
-synchronized(l899130236){
-}
-
-synchronized(l1986936160){
-}
-
-synchronized(l899130236){
-}
-
-synchronized(l1986936160){
-}
-
-}
-
-synchronized(l899130236){
-}
-
-t9.start();
-
-synchronized(l1948780723){
-}
-
-synchronized(l1948780723){
-}
-
-c768288241=true;
-synchronized(l4461550){
-synchronized(l4461550){
-synchronized(l1948780723){
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l1948780723){
-}
-
-if(c1312737936){
-synchronized(l1948780723){
-l1948780723.notify();
-}
-
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l193026137){
-}
-
-synchronized(l1948780723){
-l1948780723.notify();
-}
-
-}
-
-}
-
-synchronized(l4461550){
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l1948780723){
-}
-
-if(c768288241){
-synchronized(l1948780723){
-try{l1948780723.wait();
-}catch(InterruptedException e){e.printStackTrace();}
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l1948780723){
-}
-
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l1065330270){
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l4461550){
-synchronized(l1948780723){
-}
-
-synchronized(l193026137){
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l193026137){
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-}
-
-}
-
-synchronized(l1948780723){
-}
-
-synchronized(l1888626692){
-}
-
-synchronized(l4461550){
-synchronized(l1888626692){
-}
-
-c1312737936=true;
-synchronized(l1986936160){
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-}
-
-synchronized(l4461550){
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-if(c1312737936){
-synchronized(l1888626692){
-}
-
-synchronized(l1986936160){
-c1312737936=false;
-}
-
-synchronized(l1888626692){
-l1888626692.notify();
-}
-
-}
-
-}
-
-try{t9.join();
-}catch(InterruptedException e){e.printStackTrace();}
-}};
-static Thread t9=new Thread()
-{ public void run(){synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-c1884511064=true;
-synchronized(l4461550){
-synchronized(l4461550){
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
-
-if(c1312737936){
-synchronized(l1888626692){
-l1888626692.notify();
-}
-
-}
-
-}
-
-synchronized(l1888626692){
-}
-
-}
-
-synchronized(l4461550){
-}
-
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
+public class TraceProgram {
+	static Object l1065330270 = new Object();
+	static Object l1489977589 = new Object();
+	static Object l899130236 = new Object();
+	static Object l193026137 = new Object();
+	static Object l1434682851 = new Object();
+	static Object l1884511064 = new Object();
+	static Object l4461550 = new Object();
+	static boolean c1888626692;
+	static boolean c1970826852;
+	static boolean c1948780723;
+	static boolean c1312737936;
+	static Thread t1 = new Thread() {
+		public void run() {
+			c1312737936 = false;
+			c1970826852 = true;
+			synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@borrowObject@1104
+				synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1241
+					synchronized (l899130236) { // org.apache.commons.pool.impl.GenericKeyedObjectPool$Latch@getkey@-1
+					}
+
+					if (c1312737936) {
+						synchronized (l899130236) {
+							l899130236.notify();
+						}
+
+					}
+
+					synchronized (l899130236) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1286
+						l899130236.notify();// org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1287
+					}
+
+				}
+
+			}
+
+			if (c1970826852) {
+				synchronized (l899130236) {
+					try {
+						l899130236.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+
+			synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@borrowObject@1203
+				synchronized (l899130236) { // org.apache.commons.pool.impl.GenericKeyedObjectPool$Latch@getPool@-1
+				}
+
+			}
+
+			t9.start();
+
+			c1948780723 = true;
+			synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@borrowObject@1104
+				synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1241
+					synchronized (l1884511064) { // org.apache.commons.pool.impl.GenericKeyedObjectPool$Latch@getkey@-1
+					}
+
+					if (c1312737936) {
+						synchronized (l1884511064) {
+							l1884511064.notify();
+						}
+
+					}
+
+					synchronized (l1884511064) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1286
+						l1884511064.notify();// org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1287
+					}
+
+				}
+
+			}
+
+			if (c1948780723) {
+				synchronized (l1884511064) {
+					try {
+						l1884511064.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+
+			}
+
+			synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@borrowObject@1203
+				synchronized (l1884511064) { // org.apache.commons.pool.impl.GenericKeyedObjectPool$Latch@getPool@-1
+				}
+
+			}
+
+			synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@addObjectToPool@1565
+				synchronized (l1065330270) { // org.apache.commons.pool.impl.GenericKeyedObjectPool$ObjectQueue@decrementActiveCount@2134
+				}
+
+				synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1241
+					synchronized (l193026137) { // org.apache.commons.pool.impl.GenericKeyedObjectPool$Latch@getkey@-1
+					}
+
+					if (c1312737936) {
+						synchronized (l193026137) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1263
+							l193026137.notify();// org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1264
+						}
 
-if(c1884511064){
-synchronized(l1888626692){
-}
-
-synchronized(l1888626692){
-}
+					}
 
-synchronized(l1888626692){
-try{l1888626692.wait();
-}catch(InterruptedException e){e.printStackTrace();}
-}
-
-}
+				}
 
-synchronized(l4461550){
-}
+			}
 
-synchronized(l1888626692){
-}
+			try {
+				t9.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	};
+	static Thread t9 = new Thread() {
+		public void run() {
+			c1888626692 = true;
+			synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@borrowObject@1104
+				synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@allocate@1241
+					synchronized (l193026137) { // org.apache.commons.pool.impl.GenericKeyedObjectPool$Latch@getkey@-1
+					}
 
-if(c1884511064){
-synchronized(l1888626692){
-try{l1888626692.wait();
-}catch(InterruptedException e){e.printStackTrace();}
-}
+					if (c1312737936) {
+						synchronized (l193026137) {
+							l193026137.notify();
+						}
 
-synchronized(l1888626692){
-}
+					}
 
-}
+				}
 
-synchronized(l1888626692){
-}
+			}
 
-synchronized(l1888626692){
-}
+			if (c1888626692) {
+				synchronized (l193026137) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@borrowObject@1146
+					try {
+						l193026137.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}// org.apache.commons.pool.impl.GenericKeyedObjectPool@borrowObject@1148
+				}
 
-synchronized(l4461550){
-synchronized(l1888626692){
-}
+			}
 
-synchronized(l1986936160){
-}
+			if (c1888626692) {
+				synchronized (l193026137) {
+					try {
+						l193026137.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
 
-synchronized(l1888626692){
-}
+			}
 
-synchronized(l1986936160){
-}
+			synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@borrowObject@1203
+				synchronized (l193026137) { // org.apache.commons.pool.impl.GenericKeyedObjectPool$Latch@getPool@-1
+				}
 
-}
+			}
 
-synchronized(l1888626692){
-}
+			synchronized (l4461550) { // org.apache.commons.pool.impl.GenericKeyedObjectPool@addObjectToPool@1565
+				synchronized (l1065330270) { // org.apache.commons.pool.impl.GenericKeyedObjectPool$ObjectQueue@decrementActiveCount@2134
+				}
 
-synchronized(l4461550){
-synchronized(l1986936160){
-}
+			}
 
-synchronized(l4461550){
-}
+		}
+	};
 
+	public static void main(String[] args) {
+		t1.start();
+	}
 }
-
-}};
-public static void main(String[] args){
-t1.start();
-}}
