c64e46ed8ba98c2172e6946d04e0332f0c83d9e3
ziyilin <lin.ziyi@hotmail.com>
2015-02-04 21:43:57 +0800

    Simplify the test

--- pool149/src/Pool149.java
+++ pool149/src/Pool149.java
@@ -1,112 +1,99 @@
-
 import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.commons.pool.ObjectPool;
 import org.apache.commons.pool.PoolableObjectFactory;
 import org.apache.commons.pool.impl.GenericObjectPool;
 
-
 /**
- * Bug URL:https://issues.apache.org/jira/browse/POOL-149
- * This is a wait-notify deadlock. 
- * Reproduce environment: commons-pool-1.5, JDK 1.6.0_33.
+ * Bug URL:https://issues.apache.org/jira/browse/POOL-149 This is a wait-notify
+ * deadlock. Reproduce environment: commons-pool-1.5, JDK 1.6.0_33.
  * 
- * This test shall not reveal the deadlock reliably. The detection tools
- * can use this test to verify their ability.
+ * This test shall not reveal the deadlock reliably. The detection tools can use
+ * this test to verify their ability.
  * 
  * @collector Ziyi Lin
  */
 public class Pool149 {
 
-    public static int MAX_ACTIVE = 2;
+	public static int MAX_ACTIVE = 1;
+	private ObjectPool testPool;
 
+	public Pool149() {
+		testPool = new GenericObjectPool(new DummyPoolableObjectFactory(),
+				MAX_ACTIVE);
+	}
 
 	public void run() throws Exception {
 
-		ObjectPool testPool = new GenericObjectPool(
-				new DummyPoolableObjectFactory(), MAX_ACTIVE);
-		runOnce(testPool);
+		Object obj = testPool.borrowObject();
+
+		Thread t1 = new Thread1(obj);
+		Thread t2 = new Thread2();
+		t1.start();
+		t2.start();
+
+		t1.join();
+		t2.join();
+
 	}
 
-    private void runOnce(ObjectPool testPool) throws Exception {
-        Thread t1 = new Thread(new TestThread1(testPool));
-        Thread t2 = new Thread(new TestThread2(testPool));
+	public static void main(String[] args) throws Exception {
+		Pool149 test = new Pool149();
+		test.run();
+	}
 
-        t1.start();
-        t2.start();
+	/**
+	 * A dummy PoolableObjectFactory, creates Strings with unique names
+	 */
+	private class DummyPoolableObjectFactory implements PoolableObjectFactory {
 
-        t1.join();
-        t2.join();
-    }
+		public Object makeObject() throws Exception {
+			return "Object number-" + counter.getAndIncrement();
+		}
 
-    public static void main(String[] args) throws Exception {
-        Pool149 test = new Pool149();
-        test.run();
-    }
+		public void destroyObject(Object obj) throws Exception {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
 
-    /**
-     * A dummy PoolableObjectFactory, creates Strings with unique names
-     */
-    private class DummyPoolableObjectFactory implements PoolableObjectFactory {
+		public boolean validateObject(Object obj) {
+			return true;
+		}
 
-        public Object makeObject() throws Exception {
-            return "Object number-" + counter.getAndIncrement();
-        }
+		public void activateObject(Object obj) throws Exception {
+		}
 
-        public void destroyObject(Object obj) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
+		public void passivateObject(Object obj) throws Exception {
+			throw new UnsupportedOperationException("Not supported yet.");
+		}
 
-        public boolean validateObject(Object obj) {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
+		private final AtomicInteger counter = new AtomicInteger();
+	}
 
-        public void activateObject(Object obj) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
+	class Thread1 extends Thread {
 
-        public void passivateObject(Object obj) throws Exception {
-            throw new UnsupportedOperationException("Not supported yet.");
-        }
+		private Object object;
 
-        private final AtomicInteger counter = new AtomicInteger();
-    }
+		public Thread1(Object o) {
+			object = o;
+		}
 
-    class TestThread1 implements Runnable {
+		public void run() {
 
-        private final ObjectPool testPool;
+			try {
+				testPool.invalidateObject(object);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 
-        public TestThread1(ObjectPool testPool) {
-            this.testPool = testPool;
-        }
-
-        public void run() {
-            Object obj = null;
-            try {
-                obj = testPool.borrowObject();
-            } catch (Exception ex) {
-            } finally {
-                try {
-                    testPool.invalidateObject(obj);
-                } catch (Exception ex) {
-                }
-            }
-        }
-    }
-
-    class TestThread2 implements Runnable {
-
-        private final ObjectPool testPool;
-
-        public TestThread2(ObjectPool testPool) {
-            this.testPool = testPool;
-        }
-
-        public void run() {
-            try {
-                testPool.borrowObject();
-            } catch (Exception ex) {
-            }
-        }
-    }
+	class Thread2 extends Thread {
+		public void run() {
+			try {
+				testPool.borrowObject();
+			} catch (Exception ex) {
+			}
+		}
+	}
 }
